9ebde9065574ac14e01bcc1a9b882463d52cfd73
Olaniyi Olamide <olamideolaniyi616@gmail.com>
2025-10-17 17:56:24 +0100

    feat: add cart toggle functionality and update styles for cart display

--- script.js
+++ script.js
@@ -3,10 +3,15 @@ const body = document.querySelector("body");
 const openBtn = document.querySelector(".open-btn");
 const closeBtn = document.querySelector(".close-btn");
 const mobileNav = document.querySelector(".mobile-nav");
+const cartBtn = document.querySelector(".cart-btn");
+const cart = document.querySelector(".cart");
 
 openBtn.addEventListener("click", function () {
   mobileNav.classList.add("open-nav");
 });
 closeBtn.addEventListener("click", function () {
   mobileNav.classList.remove("open-nav");
+});
+cartBtn.addEventListener("click", function () {
+  cart.classList.toggle("opencart");
 });
