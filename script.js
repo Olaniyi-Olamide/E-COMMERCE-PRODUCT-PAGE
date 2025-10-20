413e90b9fbd6ac580ab54c3275e0d6bcf8d4834a
Olaniyi Olamide <olamideolaniyi616@gmail.com>
2025-10-20 10:41:55 +0100

    feat: added checkout functionality - added delete cart functionality - updated add to cart functionality - added orders num feature

--- script.js
+++ script.js
@@ -4,10 +4,18 @@ const openBtn = document.querySelector(".open-btn");
 const closeBtn = document.querySelector(".close-btn");
 const mobileNav = document.querySelector(".mobile-nav");
 const cartBtn = document.querySelector(".cart-btn");
+const ordersNum = document.querySelector(".orders-num");
 const cart = document.querySelector(".cart");
 const minusBtn = document.querySelector(".minus-btn");
 const plusBtn = document.querySelector(".plus-btn");
-const noOfOrders = document.querySelector(".no-of-orders");
+const ordersCount = document.querySelector(".orders-count");
+const emptyCartMsg = document.querySelector(".empty-cart-msg");
+const addToCart = document.querySelector(".add-to-cart-btn");
+const cartOrder = document.querySelector(".cart-order");
+const amountOrdered = document.querySelector(".amount-ordered");
+const totalPrice = document.querySelector(".total-price");
+const checkoutBtn = document.querySelector(".checkoutBtn");
+const deleteCartBtn = document.querySelector(".delete-cart-btn");
 
 let orders = 0;
 openBtn.addEventListener("click", function () {
@@ -21,14 +29,55 @@ cartBtn.addEventListener("click", function () {
 });
 
 minusBtn.addEventListener("click", function () {
-  if (orders === 0) return;
+  if (!orders) {
+    ordersNum.classList.add("hidden");
+  }
 
-  if (orders >= 0) {
+  if (orders > 0) {
     orders = orders - 1;
-    noOfOrders.innerHTML = orders;
+    ordersCount.innerHTML = orders;
+    ordersNum.innerHTML = orders;
   }
 });
 plusBtn.addEventListener("click", function () {
+  ordersNum.classList.remove("hidden");
   orders = orders + 1;
-  noOfOrders.innerHTML = orders;
+  ordersCount.innerHTML = orders;
+  ordersNum.innerHTML = orders;
+});
+
+addToCart.addEventListener("click", function () {
+  if (orders === 0) {
+    emptyCartMsg.classList.remove("hidden");
+    cartOrder.classList.add("hidden");
+  }
+  if (orders >= 1) {
+    emptyCartMsg.classList.add("hidden");
+    cartOrder.classList.remove("hidden");
+  }
+  amountOrdered.innerHTML = `$125.00 x ${orders}`;
+  totalPrice.innerHTML = `$${125 * orders}.00`;
+});
+
+deleteCartBtn.addEventListener("click", function () {
+  orders = 0;
+  ordersCount.innerHTML = orders;
+  emptyCartMsg.classList.remove("hidden");
+  cartOrder.classList.add("hidden");
+  ordersNum.classList.add("hidden");
+});
+
+checkoutBtn.addEventListener("click", function () {
+  cartOrder.classList.add("hidden");
+  emptyCartMsg.classList.remove("hidden");
+  ordersNum.classList.add("hidden");
+  emptyCartMsg.innerHTML = "Your order is complete 🎉";
+
+  orders = 0;
+  ordersCount.innerHTML = orders;
+
+  setTimeout(() => {
+    cart.classList.remove("opencart");
+    emptyCartMsg.innerHTML = "Your cart is empty";
+  }, 2000);
 });
